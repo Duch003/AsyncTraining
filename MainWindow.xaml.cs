3b9cd007a66ace35f9018b32a3cae4fd863961b2
Duch003 <tomaszmankin003@gmail.com>
2018-12-17 21:56:13 +0000

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncTraining
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    ///
+    //Source: https://www.youtube.com/watch?v=2moh18sh5p4
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedTime = watch.ElapsedMilliseconds;
+
+            resultWindow.Text += $"Total execution time: {elapsedTime/1000} sec.";
+        }
+
+        private async void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            await RunDownloadParallelAsync();
+
+            watch.Stop();
+            var elapsedTime = watch.ElapsedMilliseconds;
+
+            resultWindow.Text += $"Total execution time: {elapsedTime / 1000} sec.";
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            resultWindow.Text = "";
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            var websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (var site in websites)
+            {
+                tasks.Add(DownloadWebsiteAsync(site));
+            }
+
+
+            var results = await Task.WhenAll(tasks);
+
+            foreach (var item in results)
+            {
+                ReportWebsiteInfo(item);
+            }
+        }
+
+        private async Task RunDownloadParallelAsync()
+        {
+            var websites = PrepData();
+            foreach (var site in websites)
+            {
+                WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private void RunDownloadSync()
+        {
+            var websites = PrepData();
+            foreach (var site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+        private async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultWindow.Text +=
+                $"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} characters long.{Environment.NewLine}";
+        }
+    }
+}
